3ffbfcc4ca7ca1680c80f37a7e3bdc609a2d4ae2
kuba <adamiak.dev@gmail.com>
2023-01-09 11:44:57 +0100

    google fonts moved to index.html

--- src/GlobalStyles.ts
+++ src/GlobalStyles.ts
@@ -1,7 +1,7 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@500;800;900&display=swap');
+    
 
     * {
     box-sizing: border-box;
